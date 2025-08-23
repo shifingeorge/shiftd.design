c552fce3808ef15939b59a9fde6d771c1391ab23
shifiyy <shifiyy03.design@gmail.com>
2025-08-23 08:02:44 +0000

    postcss.config.js updated lmarena

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    'postcss-nesting': {}, // official nesting plugin
+    'tailwindcss/nesting': {}, // Tailwind-compatible nesting
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {}
+  }
 };
